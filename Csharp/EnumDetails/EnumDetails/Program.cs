c29dadcc44a25735a451b405d39f1fd298251441
SumeyyeKilic <sumeyye.kilic@halkbank.com.tr>
2024-01-23 17:33:21 +0300

    OOP Inheritanceout, ref and params spesific function arguments. get enum names with code. Enum details. functions details and constant and enum mechanism

---
+++ Csharp/EnumDetails/EnumDetails/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace   EnumDetails
+{
+
+    internal class Program {
+        static void Main()
+        {
+            AkrabalikTipleri akrabalikTipleri = AkrabalikTipleri.Abi |
+                                                AkrabalikTipleri.Ogul |
+                                                AkrabalikTipleri.Baba |
+                                                AkrabalikTipleri.Amca |
+                                                AkrabalikTipleri.Dayi |
+                                                AkrabalikTipleri.Dede ;
+            Console.WriteLine((int)(akrabalikTipleri));
+            
+        }
+    }
+ 
+
+    public enum AkrabalikTipleri
+    {
+        Abi=1,
+        Ogul=2, 
+        Baba=4,
+        Amca=8, 
+        Dayi=16, 
+        Dede=32
+    }
+}
---
+++ OOPOverview/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using Encapsulation;
+
+Product product = new Product();
+//product.Price = -5; 
+
+product.SetPrice(15);
+//product.IsStock = false; set edilemez
+Console.WriteLine(product.GetPrice());
+
+
+product.Description = "Test...";
+Console.WriteLine($"Açıklama: {product.Description} ");
+product.StockCount = 5;
+product.DecreaseStock(5);
+if (!product.IsStock)
+{
+    Console.WriteLine("stokta kalmadı");
+
+}
