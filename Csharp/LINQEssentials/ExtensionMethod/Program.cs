3a9e01755160e1eb06322a122c4fa3da2e42c311
SumeyyeKilic <smyyekilic@gmail.com>
2024-01-25 14:19:36 +0300

    Extension methods explained

---
+++ Csharp/LINQEssentials/ExtensionMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExtensionMethod;
+
+int x = 5;
+//x. isimli deiğişkene özel metot eklemek istiyoruz. çalışacağım değer zaten int ise bu değişkeni başkabi claasın metoduna göndermek için
+
+x.GetSquare();
+Console.WriteLine(x.GetSquare());
+string tableName = "Order Details";
+Console.WriteLine(tableName.RemoveEmptySpaces());
+
+string isim = "sümeyye kılıç";
+Console.WriteLine(isim.ToTitleCase());
+
+
+string isim2 = "mehmet ali birand";
+Console.WriteLine(isim.ToTitleCase());
+
+Console.WriteLine(new Random().NextString(8, true));
---
+++ Csharp/LINQEssentials/LINQSamples/LINQSamples/Program.cs
@@ -0,0 +1,46 @@
+﻿using LINQSamples;
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello, World!");
+
+List<Product> GetProductsByMoreThan(decimal price)
+{
+    var availableProducts = new List<Product>();
+    var products = new ProductService().GetProducts();
+    foreach (var product in products)
+    {
+        if (product.Price > price)
+        {
+            availableProducts.Add(product);
+        }
+    }
+
+    return availableProducts;
+}
+
+List<Product> GetProductsByMoreThanLinq(decimal price)
+{
+
+    var products = new ProductService().GetProducts();
+    return (
+             from product in products
+             where product.Price > price
+             orderby product.Name
+             select product
+
+            ).ToList();
+
+
+
+}
+List<Product> GetProductsByMoreThanExtensions(decimal price)
+{
+
+    var products = new ProductService().GetProducts();
+    return products.Where(product => product.Price > price)
+                   .OrderBy(product => product.Name)
+                   .ToList();
+
+
+
+}
