e8afdd4815292f8e29de8abec1432ef160de32b2
SumeyyeKilic <smyyekilic@gmail.com>
2024-01-25 16:21:47 +0300

    LINQ Samples continued

--- Csharp/LINQEssentials/LINQSamples/LINQSamples/Product.cs
+++ Csharp/LINQEssentials/LINQSamples/LINQSamples/Product.cs
@@ -6,19 +6,13 @@ using System.Threading.Tasks;
 
 namespace LINQSamples
 {
-    internal class Product
-    {
-    }
-}
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LINQSamples
-{
-    public class Product
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Product : IComparable<Product>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -29,11 +23,21 @@ namespace LINQSamples
         public int Stock { get; set; }
         public bool IsStock { get; set; }
 
+        public int CompareTo(Product? other)
+        {
+            if (Price == other?.Price) return 0;
+
+            return this.Price > other?.Price ? 1 : -1;
+
+
+
+        }
     }
 
     public class ProductService
     {
         private List<Category> _categories;
+        private Category phone = new Category() { Id = 1, Name = "Telefon" };
         public ProductService()
         {
             createCategories();
@@ -45,11 +49,11 @@ namespace LINQSamples
                 new(){ Id=2, Name = "Jeneratör", Description="Elektrik kesintisi...", Price=150000, Stock=120, IsStock=true,  Category= _categories[1]},
                 new(){ Id=3, Name = "Sunucu sistemleri", Description="Data center için..", Price=50000,  Stock=0, IsStock=false, Category= _categories[2]},
                 new(){ Id=4, Name = "Oda tipi soğutucu", Description="Soğuk Hava Deposu", Price=25000,  Stock=10,IsStock=true, Category= _categories[2]},
-                new(){ Id=5, Name ="Telefon A", Description="Android", IsStock= true, Price=8500, Category = _categories[0]},
-                new(){ Id=6, Name ="Telefon B", Description="Android", IsStock= true, Price=10500, Category = _categories[0]},
-                new(){ Id=7, Name ="Telefon C", Description="IoS", IsStock= true, Price=78500, Category = _categories[0]},
-                new(){ Id= 8, Name ="Telefon D", Description="Android", IsStock= true, Price=12500, Category = _categories[0]},
-                new(){ Id= 9, Name ="Tekstil X", Description="Tişört", IsStock= true, Price=500, Category = _categories[4]},
+                new(){ Id=5, Name ="Telefon A", Description="Android", IsStock= true, Price=8500, Category = phone},
+                new(){ Id=6, Name ="Telefon B", Description="Android", IsStock= true, Price=10500, Category = phone},
+                new(){ Id=7, Name ="Telefon C", Description="IoS", IsStock= true, Price=78500, Category = phone},
+                new(){ Id= 8, Name ="Telefon D", Description="Android", IsStock= true, Price=12500, Category = phone},
+                new(){ Id= 9, Name ="Tekstil X", Description="Tişört", IsStock= true, Price=500, Category = phone},
 
 
 
@@ -59,7 +63,8 @@ namespace LINQSamples
         {
             _categories = new List<Category>()
             {
-                new(){ Id=1, Name="Telefon"},
+                //new(){ Id=1, Name="Telefon"},
+                phone,
                 new(){ Id=2, Name="Enerji"},
                 new(){ Id=3, Name="Data Center"},
                 new(){ Id=4, Name="Kozmetik"},
@@ -67,6 +72,9 @@ namespace LINQSamples
             };
 
         }
+
+        public List<Category> GetCategories() => _categories;
+
 
     }
 }
